b91ce647eadb07f0b0f5908931e8a85da7120c4d
Hugo Arregui <hugo@properati.com>
2016-01-25 17:38:01 -0300

    make linter.run return the parsed error

--- git-partial-lint.py
+++ git-partial-lint.py
@@ -7,6 +7,7 @@ from os import path
 from lib.git import GitInterface
 from lib.rules import find_linter, LinterNotFound
 
+
 def run_linter(linter, f):
     git = GitInterface()
     current_brach = git.current_brach()
@@ -17,10 +18,11 @@ def run_linter(linter, f):
     blame = git.blame_file(f) if not is_new_file else None
     out = []
     for error in linter_output:
-        if is_new_file or (linter.error_linenum(error) in blame):
+        if is_new_file or (error['linenum'] in blame):
             s = linter.format_error(error)
             out.append(s)
     return out
+
 
 def main():
     argparser = argparse.ArgumentParser(
--- lib/rules.py
+++ lib/rules.py
@@ -11,34 +11,44 @@ class Eslint:
     def run(self, f):
         out = run(["eslint", f],
                   stdout=PIPE).stdout.decode('utf-8').splitlines()
-        return out[2:-3]
-
-    def error_linenum(self, error):
-        return int(error.split(':')[0])
+        r = []
+        if len(out) > 5:
+            for line in out[2:-3]:
+                fields = [f.strip() for f in line.split(':')]
+                r.append({
+                    'file': f,
+                    'linenum': int(fields[0]),
+                    'type': fields[1],
+                    'err': fields[-1],
+                    'where': "{:s}".format(fields[0]),
+                    'desc': " ".join(fields[2:-1])
+                })
+        return r
 
     def format_error(self, error):
-        fields = [f.strip() for f in error.split(':')]
-        where, type = fields[:2]
-        desc = " ".join(fields[2:-1])
-        err = fields[-1]
-        return "{:>8s} {:>7s} {:>50s} {:s}".format(where, type, desc, err)
+        return "{where:>8} {type:>7} {desc:>50} {err}".format(**error)
 
 
 class Flake8:
     name = 'flake8'
 
     def run(self, f):
-        return run(["flake8", f],
-                   stdout=PIPE).stdout.decode('utf-8').splitlines()
-
-    def error_linenum(self, error):
-        return int(error.split(':')[1])
+        out = run(["flake8", f],
+                  stdout=PIPE).stdout.decode('utf-8').splitlines()
+        r = []
+        for line in out:
+            fields = [f.strip() for f in line.split(':')]
+            r.append({
+                'file': f,
+                'linenum': int(fields[1]),
+                'colnum': int(fields[2]),
+                'err': fields[-1],
+                'where': "{:s}:{:s}".format(fields[1], fields[2])
+            })
+        return r
 
     def format_error(self, error):
-        fields = [f.strip() for f in error.split(':')]
-        where = "{:s}:{:s}".format(fields[1], fields[2])
-        err = fields[-1]
-        return "{:>8s} {:s}".format(where, err)
+        return "{where:>8} {err}".format(**error)
 
 
 class Rubocop:
@@ -46,18 +56,23 @@ class Rubocop:
 
     def run(self, f):
         out = run(['rubocop', '-f', 's', f],
-                   stdout=PIPE).stdout.decode('utf-8').splitlines()
-        return out[1:-2] if len(out) > 3 else []
-
-    def error_linenum(self, error):
-        return int(error.split(':')[1])
+                  stdout=PIPE).stdout.decode('utf-8').splitlines()
+        r = []
+        if len(out) > 3:
+            for line in out[1:-2]:
+                fields = [f.strip() for f in line.split(':')]
+                r.append({
+                    'file': f,
+                    'type': fields[0],
+                    'linenum': int(fields[1]),
+                    'colnum': int(fields[2]),
+                    'err': fields[-1],
+                    'where': "{:s}:{:s}".format(fields[1], fields[2])
+                })
+        return r
 
     def format_error(self, error):
-        fields = [f.strip() for f in error.split(':')]
-        where = "{:s}:{:s}".format(fields[1], fields[2])
-        type = fields[0]
-        err = fields[-1]
-        return "{:>8s} [{:s}] {:s}".format(where, type, err)
+        return "{where:>8} [{type}] {err}".format(**error)
 
 
 linters = [Eslint(), Flake8(), Rubocop()]
